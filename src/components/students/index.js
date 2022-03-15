38c46b638e8d031ea5a17c27b85f5deebd16e8da
brewchetta <chett.tiller@gmail.com>
2022-03-15 18:10:51 -0400

    implemented local storage for students and cohort names

--- src/components/students/index.js
+++ src/components/students/index.js
@@ -1,13 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import StudentForm from './StudentForm'
 import StudentClassList from "./StudentClassList"
 import StudentDetail from "./StudentDetail"
+import * as local from 'utils/localStorageUtils'
 
 function StudentList(props) {
 
   const [students, setStudents] = useState([])
-  const [classNames, setClassNames] = useState(new Set())
+  const [classNames, setClassNames] = useState(new Set([]))
   const [inspectedStudent, setInspectedStudent] = useState(null)
+
+  useEffect(() => {
+    const cohorts = new Set(local.getCohortNames())
+    setClassNames(cohorts)
+    cohorts.forEach(c => {
+      const localStudents = local.getStudents(c)
+      setStudents(prev => [...prev, ...localStudents])
+    })
+  }, [])
+
+  useEffect(() => {
+    local.setCohortNames(Array.from(classNames))
+  }, [classNames])
 
   function filterStudentsByClassName(className) {
     return students.filter(s => s.className === className)
@@ -16,6 +30,8 @@ function StudentList(props) {
   function addStudent(student) {
     setStudents(prev => [...prev, student])
     setClassNames(prev => new Set([...prev, student.className]))
+    const prevStudents = local.getStudents(student.className) || []
+    local.setStudents(student.className, [...prevStudents, student])
   }
 
   function removeStudent(student) {
@@ -23,6 +39,8 @@ function StudentList(props) {
       setInspectedStudent(null)
     }
     setStudents(prev => prev.filter(s => s !== student))
+    const prevStudents = local.getStudents(student.className)
+    local.setStudents(student.className, prevStudents.filter(s => s.id !== student.id))
   }
 
   const renderedClassLists = Array.from(classNames).map(cN => (
--- src/utils/localStorageUtils.js
+++ src/utils/localStorageUtils.js
@@ -2,10 +2,23 @@ export function getStudents (className='no-class') {
   return JSON.parse(localStorage.getItem(`students-${className}`))
 }
 
-export function setStudents(students, className='no-class') {
+export function setStudents(className='no-class', students) {
   return localStorage.setItem(`students-${className}`, JSON.stringify(students))
 }
 
 export function clearStudents(className='no-class') {
-  return localStorage.removeItem('students')
+  return localStorage.removeItem(`students-${className}`)
+}
+
+export function getCohortNames() {
+  const cohorts = JSON.parse(localStorage.getItem('cohort-names'))
+  return cohorts[0] ? cohorts : []
+}
+
+export function setCohortNames(cohortNames) {
+  return localStorage.setItem('cohort-names', JSON.stringify(cohortNames))
+}
+
+export function clearCohortNames() {
+  return localStorage.removeItem('cohort-names')
 }
