8d44df8b1e4e4a43414cbd2356853b054b74fd0b
brewchetta <chett.tiller@gmail.com>
2022-08-09 17:25:10 -0400

    reformatted code for group matching to be easier to read, added prevGroup optional argument)

--- src/utils/arrayUtils.js
+++ src/utils/arrayUtils.js
@@ -1,17 +1,27 @@
 export const randomArrayItem = arr => arr[Math.floor(Math.random() * arr.length)]
 
-export function buildGroupsFromArray(array, groupSize=2) {
+export function buildGroupsFromArray( array, groupSize=2, prevGroup=[] ) {
+
   let ungrouped = [...array]
+
   const groups = []
+
   while (ungrouped.length > 0 && groupSize > 0) {
+
     const randomParticipant = randomArrayItem(ungrouped)
+
     ungrouped = ungrouped.filter(s => s !== randomParticipant)
+
     const lastGroupCreated = groups[groups.length - 1]
+
     if (groups.length && (ungrouped.length === 0 || groups[groups.length - 1].length < groupSize)) {
       lastGroupCreated.push(randomParticipant)
     } else {
       groups.push([randomParticipant])
     }
-  }
+
+  } //end while
+
   return groups
+
 }
